e797d5708013c47ae95ac3658f3dc3002c01f079
lsndr <lsndrme@ya.ru>
2025-05-20 00:03:34 +0500

    test: add tests for off methods

--- tests/core/context.spec.ts
+++ tests/core/context.spec.ts
@@ -45,4 +45,33 @@ describe('Core – Context', () => {
 
     expect(onBeforeFlush).toHaveBeenCalledBefore(onAfterFlush);
   });
+
+  it('should not emit flush events if unsubscribed', async () => {
+    // arrange
+    const entity = { id: faker.string.uuid(), name: faker.person.fullName() };
+    const onBeforeFlush = jest.fn();
+    const onAfterFlush = jest.fn();
+    uow.on('beforeRun', ({ context }) => {
+      context.on('beforeFlush', onBeforeFlush);
+      context.on('afterFlush', onAfterFlush);
+      context.off('beforeFlush', onBeforeFlush);
+      context.off('afterFlush', onAfterFlush);
+    });
+
+    // act
+    await uow.run((ctx) => {
+      const repositoryMock = ctx.getRepository(RepositoryMock);
+
+      repositoryMock.doInsert.mockResolvedValue(true);
+      repositoryMock.extractIdentity.mockResolvedValue(entity.id);
+
+      repositoryMock.add(entity);
+
+      return repositoryMock;
+    });
+
+    // assert
+    expect(onBeforeFlush).not.toHaveBeenCalled();
+    expect(onAfterFlush).not.toHaveBeenCalled();
+  });
 });
