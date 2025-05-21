e25bc29d6f2dee1d4ceb4f1319793adbfc05d9fd
Alexander <lsndrme@ya.ru>
2025-05-21 21:22:04 +0700

    chore(eslint): fix config (#153)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,56 @@
 import tseslint from 'typescript-eslint';
-import eslintComments from '@eslint-community/eslint-plugin-eslint-comments';
-import importPlugin from 'eslint-plugin-import';
-import { fixupPluginRules } from '@eslint/compat';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
+import eslintJs from '@eslint/js';
+import * as importPlugin from 'eslint-plugin-import-x';
+import commentsPluginConfigs from '@eslint-community/eslint-plugin-eslint-comments/configs';
 
-export default tseslint.config(
+export default defineConfig(
   {
-    ignores: ['node_modules/**/*', 'dist/**/*'],
+    ignores: ['node_modules', './dist', './coverage'],
   },
-  tseslint.configs.recommendedTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
-  importPlugin.flatConfigs.recommended,
   {
-    plugins: {
-      eslintComments: fixupPluginRules(eslintComments),
-      importPlugin: importPlugin,
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
     },
+  },
+  commentsPluginConfigs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    extends: [
+      eslintJs.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 'latest',
+    },
+    settings: {
+      'import-x/resolver-next': [importPlugin.createNodeResolver()],
+    },
+  },
+  {
+    files: ['**/*.ts'],
     languageOptions: {
       parserOptions: {
         project: './tsconfig.eslint.json',
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+    extends: [
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    settings: {
+      'import-x/resolver-next': [
+        createTypeScriptImportResolver({
+          tsconfig: './tsconfig.eslint.json',
+        }),
+      ],
     },
     rules: {
       '@typescript-eslint/explicit-module-boundary-types': 'error',
@@ -28,6 +59,8 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
+      'import-x/no-default-export': 'error',
+      '@eslint-community/eslint-comments/require-description': 'error',
       '@typescript-eslint/consistent-type-imports': [
         'error',
         {
@@ -65,13 +98,5 @@ export default tseslint.config(
         },
       ],
     },
-    settings: {
-      'import/resolver': {
-        typescript: {
-          project: './tsconfig.eslint.json',
-        },
-      },
-    },
   },
-  {},
 );
